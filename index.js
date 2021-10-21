ea0e793e78d1929e116f158955c14a7ab42e054a
unknown <sseryun2@naver.com>
2021-10-21 10:34:16 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://seryun:seryun@boilerplate.oadzx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(() => console.log('MongoDB Connected..'))
+    .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello Wolrd! 안녕하세요~ '))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
